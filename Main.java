7c149ee7f23936c231523b6352c9f70135993da7
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-03-30 17:21:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
+/**
+ * This class represents the entry point of the Blackjack game.
+ */
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("""
-                Welcome to Blackjack!
-                 _____     _____     _____     _____
-                |A    |   |A    |   |A    |   |A    |
-                |♠   ♠|   |♣   ♣|   |♥   ♥|   |♦   ♦|
-                |  ♠  |   |  ♣  |   |  ♥  |   |  ♦  |
-                | ♠ ♠ |   | ♣ ♣ |   | ♥ ♥ |   | ♦ ♦ |
-                |____V|   |_____|   |_____|   |_____|
-                """);
 
+public class Main {
+    /**
+     * The main method of the game.
+     */
+    public static void main(String[] args) {
+        // Display welcome message and prompt for number of players
+        System.out.println("""
+            Welcome to Blackjack!
+             _____     _____     _____     _____
+            |A    |   |A    |   |A    |   |A    |
+            |♠   ♠|   |♣   ♣|   |♥   ♥|   |♦   ♦|
+            |  ♠  |   |  ♣  |   |  ♥  |   |  ♦  |
+            | ♠ ♠ |   | ♣ ♣ |   | ♥ ♥ |   | ♦ ♦ |
+            |_____|   |_____|   |_____|   |_____|
+            """);
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many players?: ");
         int numPlayers = scanner.nextInt();
         System.out.println();
+
+        // Start the game
         new Blackjack(numPlayers);
     }
 }
